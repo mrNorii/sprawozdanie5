cecb6c061d32dbccdb7f9f88f57f6c19d97b4e61
mrNorii <ksabiron03@gmail.com>
2022-01-27 20:52:35 +0100

    after hard working with CORS finally i red date from my API

--- src/App.js
+++ src/App.js
@@ -3,20 +3,32 @@ import Axios from 'axios';
 import "./App.css";
 
 function App() {
-  const [user, setUser] = useState("");
+  const [userCat, setJokeCat] = useState("");
+  const [userSet, setJokeSet] = useState("");
+  const [userDel, setJokeDelivery] = useState("");
 
   const stala = () => {
-    Axios.get("https://v2.jokeapi.dev/joke/Any?safe-mode")
+    Axios.get("http://127.0.0.1:8000/modele/api/modeles/?format=json")
     .then((response) => {
         console.log(response);
-        setUser(response.data.category + " | " + response.data.setup + " ... " + response.data.delivery);
+        /*setJokeCat(response.data.category)
+        setJokeSet(response.data.setup)
+        setJokeDelivery(response.data.delivery);*/
     }
   );
 };
 return (
   <div>
     Click Button <button onClick={stala}>Get Joke</button><br></br>
-    {user}
+    <div id='Category'>
+      {userCat}
+    </div>
+    <div id='Setup'>
+      {userSet}
+    </div>
+    <div id='Delivery'>
+      {userDel}
+    </div>
   </div>
   );
 }
--- src/ModeleAut.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function ModeleAut() {
-  return (
-  <div>
-      asd
-  </div>
-  )
-}
