244faec9320bd75b276601b17f3ebb976bf22d8e
mrNorii <ksabiron03@gmail.com>
2022-01-27 14:44:45 +0100

    delete useless png

--- src/App.js
+++ src/App.js
@@ -1,7 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return null
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      Marka: {}
+    }
+  }
+
+  componentDidMount() {
+    fetch("http://127.0.0.1:8000/modele/api/modeles/1")
+      .then(response => response.json())
+      .then(data => {
+        this.setState({
+          Marka: data
+        })
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        asd
+      </div>
+    )     
+  }
+
 }
+//{this.state.Model.name}
 
 export default App;
