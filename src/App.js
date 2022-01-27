a318c30ba876b33c8e838e7a7ac6785581ec9b5f
mrNorii <ksabiron03@gmail.com>
2022-01-27 21:28:47 +0100

    display 1st car

--- src/App.js
+++ src/App.js
@@ -3,34 +3,28 @@ import Axios from 'axios';
 import "./App.css";
 
 function App() {
-  const [userCat, setJokeCat] = useState("");
-  const [userSet, setJokeSet] = useState("");
-  const [userDel, setJokeDelivery] = useState("");
+  const [car, setCar] = useState("");
 
   const stala = () => {
     Axios.get("http://127.0.0.1:8000/modele/api/modeles/?format=json")
     .then((response) => {
         console.log(response);
-        /*setJokeCat(response.data.category)
-        setJokeSet(response.data.setup)
-        setJokeDelivery(response.data.delivery);*/
+        setCar(response.data[0].Marka + " " + response.data[0].Model + " " + response.data[0].Opis + " " + response.data[0].Pojemnosc_Silnika + " "
+        + response.data[0].Przebieg + " " + response.data[0].Rodzaj_Paliwa + " " + response.data[0].Rok)
     }
   );
 };
 return (
   <div>
-    Click Button <button onClick={stala}>Get Joke</button><br></br>
+    View Cars <button onClick={stala}>PRIVIET</button><br></br>
     <div id='Category'>
-      {userCat}
-    </div>
-    <div id='Setup'>
-      {userSet}
-    </div>
-    <div id='Delivery'>
-      {userDel}
+      {car}
     </div>
   </div>
   );
 }
 
 export default App;
+
+//setCar(response.data[0].Marka + " " + response.data[0].Model + " " + response.data[0].Opis + " " + response.data[0].Pojemnosc_Silnika + " "
+//+ response.data[0].Przebieg + " " + response.data[0].Rodzaj_Paliwa + " " + response.data[0].Rok)
