289e820051a831cd0f8e37a6ca606a9c1bba0063
mrNorii <ksabiron03@gmail.com>
2022-01-28 01:47:57 +0100

    css complete

--- src/App.js
+++ src/App.js
@@ -27,13 +27,27 @@ export default class App extends React.Component{
 
   const EveryCar = this.state.Cars.map(Car => (
     <div id='box' key={Car.id}>
-      <div id='marka'>{Car.Marka}</div>
-      <div id='model'>{Car.Model}</div>
-      <div id='opis'>{Car.Opis}</div>
-      <div id='pojemnosc'>{Car.Pojemnosc_Silnika}</div>
-      <div id='przebieg'>{Car.Przebieg}</div>
-      <div id='rodzaj'>{Car.Rodzaj_Paliwa}</div>
-      <div id='rok'>{Car.Rok}</div> 
+      <div id='left'>
+        <div id='markaL'><label id='text'>Marka: </label></div>
+        <div id='modelL'><label id='text'>Model: </label></div>
+        <div id='rokL'><label id='text'>Rok: </label></div>
+        <div id='przebiegL'><label id='text'>Przebieg: </label></div>
+        <div id='pojemnoscL'><label id='text'>Pojemność Silnika: </label></div>
+        <div id='rodzajL'><label id='text'>Rodzaj Paliwa: </label></div>
+        <div id='opisL'><label id='text'>Opis: </label></div>
+      </div>
+      <div id='right'>
+        <div id='marka'>{Car.Marka}</div>
+        <div id='model'>{Car.Model}</div>
+        <div id='rok'>{Car.Rok}</div>
+        <div id='przebieg'>{Car.Przebieg} km</div>
+        <div id='pojemnosc'>{Car.Pojemnosc_Silnika} cm3</div>
+        <div id='rodzaj'>{Car.Rodzaj_Paliwa}</div>
+        <div id='opis'>{Car.Opis}</div>
+      </div>
+      <div id='right2'>
+        <img src={Car.Zdjecie} id='zdjecie' width="400px" />
+      </div>
     </div>
     ));
 
